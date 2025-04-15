fc149332688d25644140242a9d1f34230a9a6cf4
Filipe <filipe.candido.rodrigues@gmail.com>
2025-04-14 21:53:20 -0300

    finalizado função de filtrar tarefas

--- js/scripts.js
+++ js/scripts.js
@@ -86,6 +86,27 @@ const getSearchTodos = (search) => {
     });
 };
 
+//função filtro todos
+
+const filterTodos = (filterValue) =>{
+    const todos =document.querySelectorAll(".todo");
+
+    switch(filterValue){
+        case"all":
+        todos.forEach((todo) => (todo.style.display = "flex"));
+        break;
+        case"done":
+        todos.forEach((todo) => todo.classList.contains("done") ? todo.style.display ="flex" : todo.style.display ="none");
+        break;
+        case"todo":
+        todos.forEach((todo) => ! todo.classList.contains("done") ? todo.style.display ="flex" : todo.style.display ="none");
+        break;
+        default:
+            break;
+    }
+
+}
+
 //Eventos
 todoForm.addEventListener("submit",(e) =>{
         e.preventDefault();
@@ -160,3 +181,11 @@ eraseBtn.addEventListener ("click", (e) =>{
     searchInput.value = ""//zerar o valor de search
     searchInput.dispatchEvent(new Event("keyup")); //voltar com os inputs
 });
+
+//evento de filtrar as tarefes
+
+filterBtn.addEventListener("change", (e) => {
+    const filterValue =e.target.value;
+    
+    filterTodos(filterValue);
+})
